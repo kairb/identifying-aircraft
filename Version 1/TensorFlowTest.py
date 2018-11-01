28b3c107695bbc17095f1af0469b001eced87072
Kai <kairoperblackman@gmail.com>
2018-11-01 18:42:50 +0000

    Initial commit of simple Tensorflow test

--- Version 1/TensorFlowTest.py
+++ Version 1/TensorFlowTest.py
@@ -1,8 +1,6 @@
 # TensorFlow and Keras imports
 import tensorflow as tf
 from tensorflow import keras
-import random
-
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,18 +21,7 @@ train_images = train_images / 255.0
 
 test_images = test_images / 255.0
 
-# First 25 images of dataset are displayed with appropriate lables
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(train_images[i], cmap=plt.cm.binary)
-#     plt.xlabel(class_names[train_labels[i]])
-# plt.show()
-
-# contstruct model
+# construct model
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
