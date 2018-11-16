aa32e5aeafcff291916720d7b57832d3584cdb14
Kai <kairoperblackman@gmail.com>
2018-11-16 16:40:38 +0000

    scikit-learn test

--- Version 1/Scikitlearn.py
+++ Version 1/Scikitlearn.py
@@ -1,2 +1,20 @@
+#import statements
 from sklearn import datasets
+from sklearn import svm
 
+import matplotlib.pyplot as plt
+
+
+digits = datasets.load_digits()
+
+
+clf = svm.SVC(gamma="auto", C=100)
+
+x,y = digits.data[:-1], digits.target[:-1]
+clf.fit(x,y)
+
+#print("Prediction: ", clf.predict(digits.data[-1]))
+
+plt.imshow(digits.images[-1], cmap = plt.cm.gray_r, interpolation = "nearest")
+
+plt.show()
