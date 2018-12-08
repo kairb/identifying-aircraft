00514a5f4d8831db6170b1ba014a713bd9e4c03e
Kai <kairoperblackman@gmail.com>
2018-12-08 20:35:50 +0000

    Created requirements.bat to install dependencies

--- Version 1/classifier.py
+++ Version 1/classifier.py
@@ -1,25 +1,27 @@
 # import statements
 from sklearn import svm
-from sklearn import datasets
 import matplotlib.pyplot as plt
 from image_parser import Parser
 import numpy as np
 from data_parser import Data
 
-clf = svm.OneClassSVM()
+clf = svm.OneClassSVM(kernel="rbf", gamma=0.1, nu=0.5)
 data = Data()
+TRAINING_SET_SIZE = 90
+TEST_IMAGE = 93
 
 ##get training data
-data.new_training_set(90)
+data.new_training_set(TRAINING_SET_SIZE)
 training_set = data.get_training_set()
 training_labels = data.get_training_labels()
-test_image = data.get_test_image(102)
+test_image = data.get_test_image(TEST_IMAGE)
 
+##fit training data
 clf.fit(training_set, training_labels)
 
-print("test image shape", test_image.shape)
 print("Prediction: ", clf.predict(test_image))
 
-plt.imshow(Parser.load_image(90))
+plt.imshow(Parser.load_image(TEST_IMAGE))
 
 plt.show()
+
