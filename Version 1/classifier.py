5a4dd3aa8a3438b72bbe8b1292739f3f98198253
Kai <kairoperblackman@gmail.com>
2019-02-05 16:39:51 +0000

    Created matplotlib pyplot gui that appears showing prediction results.

--- Version 1/classifier.py
+++ Version 1/classifier.py
@@ -6,27 +6,30 @@ from data_parser import Data
 
 # declaring data set paramaters
 TRAINING_SET_SIZE = 90
-TEST_IMAGES = [95, 195, 96, 97, 198]
+TEST_IMAGES = [95, 195, 96, 97, 198, 199, 98, 99, 88, 191]
 
 # get training data in the form of feature descriptors
 training_set, training_labels = Data.create_hog_data_set(TRAINING_SET_SIZE)
 test_images = Data.create_hog_image(TEST_IMAGES)
+# GUI
+MAX_COLUMNS = 5
+MAX_ROWS = 2
 
-clf = svm.SVC(gamma=0.0001, C=10, probability=True)
+clf = svm.SVC(gamma=0.0001, C=10)
 
 # fit training data
 clf.fit(training_set, training_labels)
-# print(clf.predict_proba(training_set))
 
 result = clf.predict(test_images)
-print("Prediction: ", clf.predict(test_images))
+print("Prediction: ", result)
 
-MAX_COLUMNS = 5
-MAX_ROWS = len(result)/MAX_COLUMNS
-
-
-
-
-plt.imshow(Parser.load_image(TEST_IMAGES[0]))
+for i in range(1, len(test_images) + 1):
+    plt.subplot(MAX_ROWS, MAX_COLUMNS, i)
+    plt.imshow(Parser.load_full_size_image(TEST_IMAGES[i - 1]), cmap="gray")
+    if result[i - 1] == 1:
+        plt.title("Aircraft")
+    else:
+        plt.title("Ground")
+    plt.axis('off')
 
 plt.show()
