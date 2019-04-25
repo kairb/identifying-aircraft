38aa7e190ffacce27a4dab3ccda5351419534a02
Kai <kairoperblackman@gmail.com>
2019-04-25 18:41:58 +0100

    changes to cross validation

--- Version 1/cross_validation.py
+++ Version 1/cross_validation.py
@@ -5,20 +5,24 @@ from sklearn.svm import SVC
 from data_parser import Data
 from sklearn.model_selection import cross_val_score
 
-sizes = [400,300]
-for size in sizes:
 
-    # training_set, training_labels = Data.create_training_data(size, size)
-    training_set, training_labels = Data.create_realistic_hog_data_set(90)
-    classifier = SVC(C=10, gamma=0.0001)
-    scores = cross_val_score(classifier, training_set, training_labels, cv=10)
-    # print(size, "x", size)
+def cross_validate():
+    sizes = [400,300]
+    for size in sizes:
 
-    total = 0
-    for score in scores:
-        total += score
+        # training_set, training_labels = Data.create_training_data(size, size)
+        training_set, training_labels = Data.create_realistic_hog_data_set(90)
+        classifier = SVC(C=10, gamma=0.0001)
+        scores = cross_val_score(classifier, training_set, training_labels, cv=10)
+        # print(size, "x", size)
 
-    print(scores)
-    print("Average: ", total / len(scores), "\n")
-    scores = 0
-    classifier = 0
+        total = 0
+        for score in scores:
+            total += score
+
+        print(scores)
+        print("Average: ", total / len(scores), "\n")
+        scores = 0
+        classifier = 0
+
+cross_validate()
